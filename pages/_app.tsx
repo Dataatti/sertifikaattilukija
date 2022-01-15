ff5b38200b3a162f40fca86d584e91f9dca31e70
Petro Silenius <petro.silenius@gmail.com>
2022-01-15 23:15:14 +0200

    Lint all files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,16 @@ import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<title>Sertifikaattilukija</title>
-				<meta
-					name="description"
-					content="Matkailualan sertifikaattilukija"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Sertifikaattilukija</title>
+        <meta name="description" content="Matkailualan sertifikaattilukija" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,30 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	return (
-		<div>
-			<main>
-				<h1>Matkailualan sertifikaattilukija</h1>
+  return (
+    <div>
+      <main>
+        <h1>Matkailualan sertifikaattilukija</h1>
 
-				<form onSubmit={(e) => e.preventDefault()}>
-					<input
-						type="search"
-						placeholder="Yrityksen nimi / y-tunnus tai sertifikaatti tai kunta"
-					/>
-					<button type="submit">Hae</button>
-				</form>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="search"
+            placeholder="Yrityksen nimi / y-tunnus tai sertifikaatti tai kunta"
+          />
+          <button type="submit">Hae</button>
+        </form>
 
-				<footer>
-					<p>
-						Business Finlandin mukaan sertifiointi on laadun tae
-						kansainvälisessä kaupassa. Matkailualan monimuotoisuus
-						on kuitenkin mahdollistanut sen, että myös sertifikaatin
-						tarjoajia on hyvin paljon. Ideana on tuottaa nyt
-						hankalasti ja pirstoutuneena oleva tieto yhteen paikkaan
-						kaikkien toimijoiden avoimesti saataville.
-					</p>
-				</footer>
-			</main>
-		</div>
-	);
+        <footer>
+          <p>
+            Business Finlandin mukaan sertifiointi on laadun tae kansainvälisessä kaupassa.
+            Matkailualan monimuotoisuus on kuitenkin mahdollistanut sen, että myös sertifikaatin
+            tarjoajia on hyvin paljon. Ideana on tuottaa nyt hankalasti ja pirstoutuneena oleva
+            tieto yhteen paikkaan kaikkien toimijoiden avoimesti saataville.
+          </p>
+        </footer>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
