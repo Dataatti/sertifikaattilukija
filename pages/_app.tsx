28e7ec4e414d0587991b9f6af7c31bb14f62b1a6
Petro Silenius <petro.silenius@gmail.com>
2022-01-15 23:42:32 +0200

    Install mui & emotion, setup emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,30 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import createCache from '@emotion/cache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createCache({ key: 'css' });
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <title>Sertifikaattilukija</title>
         <meta name="description" content="Matkailualan sertifikaattilukija" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
