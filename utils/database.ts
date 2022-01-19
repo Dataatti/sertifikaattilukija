dd9b8b8c1d655cd1045c67b146c31943f592d30e
Aleksi Väisänen <aleksi.vaisanen@loikka.io>
2022-01-19 18:37:41 +0200

    SER-3 SER-14 Add database client and initDatabase function

---
+++ utils/database.ts
@@ -0,0 +1,27 @@
+import knex from 'knex';
+/**
+ * Database client using knex
+ */
+export const dbClient = knex({
+  client: 'pg',
+  connection: process.env.DATABASE_CONNECTION_URL,
+  searchPath: ['knex', 'public'],
+});
+
+/**
+ * Function for initializing database tables
+ */
+export const initDatabase = async () => {
+  try {
+    await dbClient.schema.createTable('company', (table) => {
+      table.increments('id');
+      table.string('name');
+      table.string('vat-number');
+      table.string('address');
+      table.string('city');
+      table.specificType('certificates', 'text[]');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
