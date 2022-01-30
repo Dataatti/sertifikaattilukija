cfabcc22e435f5d36abec13c6d2d246aff68485f
Matias <matias.lappalainen@loikka.io>
2022-01-30 21:52:59 +0200

    Add manual trigger to sync_certificates

--- pages/api/sync/sync.ts
+++ pages/api/sync/sync.ts
@@ -1,10 +1,13 @@
 import type { NextApiResponse } from 'next';
 import { getCompanyInformation } from 'services/company';
+import { checkCors, corsOptions } from 'utils/cors';
 import { databaseHoc, NextRequestWithDb } from 'utils/database';
 import { getErrorMessage } from 'utils/utils';
 
 export const handler = async (req: NextRequestWithDb, res: NextApiResponse) => {
   try {
+    await checkCors(req, res, corsOptions);
+
     // 1. step
     const ok = await getCompanyInformation(req.db);
     res.status(200).json(ok);
