8c2431cff2d5716df07bb161b30a75af4230b615
Petro Silenius <petro.silenius@gmail.com>
2022-01-30 22:45:36 +0200

    Set primary color to match visit finland brand

--- theme.ts
+++ theme.ts
@@ -1,5 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({});
+const theme = createTheme({
+    palette: {
+        primary: {
+            main:'#F97C80',
+        }
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                },
+                '& .MuiSvgIcon-root': {
+                    marginRight: '5px',
+                }
+            },
+        },
+    },
+});
 
 export default theme;
