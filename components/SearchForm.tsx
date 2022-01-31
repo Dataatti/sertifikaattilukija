93b904cddf665eed479c8efaab81ce80ef7ce956
Petro Silenius <petro.silenius@gmail.com>
2022-01-31 18:17:45 +0200

    Add multiple options to cert/city filter, refactor search form to own component

---
+++ components/SearchForm.tsx
@@ -0,0 +1,100 @@
+import { Alert, Autocomplete, Button, Grid, TextField, Typography } from '@mui/material';
+import { ChangeEvent, useEffect, useState, useRef } from 'react';
+import cities from 'enums/cities.json';
+import certificates from 'enums/certificates.json';
+import { Search } from '@mui/icons-material';
+
+const SearchForm = ({
+  setCompanies,
+  setResultTotal,
+}: {
+  setCompanies: () => void;
+  setResultTotal: () => void;
+}) => {
+  const [company, setCompany] = useState('');
+  const [certs, setCerts] = useState([]);
+  const [areas, setAreas] = useState([]);
+
+  const onSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let query = [];
+
+    if (company !== '') {
+      query.push(`name=${company}`);
+    }
+    if (certs !== []) {
+      const certIds = certs.map((cert) => cert.id);
+      query.push(`certificate=${certIds}`);
+    }
+    if (areas !== []) {
+      query.push(`city=${areas}`);
+    }
+
+    const result = await fetch(`/api/data?${query.join('&')}`);
+    const { data, totalResults } = await result.json();
+    setCompanies(data);
+    setResultTotal(totalResults);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Typography component="h2" variant="h6" sx={{ pt: '10px' }}>
+        Rajaa tuloksia
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        sx={{ pt: '5px', pb: '15px', borderBottom: '1px solid #C4C4C4' }}
+      >
+        <Grid item xs={7} sm={4}>
+          <TextField
+            id="name"
+            value={company}
+            onChange={(event) => setCompany(event.target.value)}
+            fullWidth
+            label="Yrityksen nimi tai y-tunnus"
+          />
+        </Grid>
+        <Grid item xs={5} sm={4}>
+          <Autocomplete
+            id="certificate"
+            value={certs}
+            onChange={(event, newValue) => {
+              setCerts([...newValue]);
+            }}
+            multiple
+            limitTags={1}
+            disableClearable
+            fullWidth
+            options={certificates}
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => <TextField {...params} label="Sertifikaatti" />}
+          />
+        </Grid>
+        <Grid item xs={8} sm={3}>
+          <Autocomplete
+            id="city"
+            value={areas}
+            onChange={(event, newValue) => {
+              setAreas([...newValue]);
+            }}
+            multiple
+            limitTags={2}
+            disableClearable
+            fullWidth
+            options={cities.cities}
+            renderInput={(params) => <TextField {...params} label="Kaupunki" />}
+          />
+        </Grid>
+        <Grid item xs={4} sm={1}>
+          <Button type="submit" variant="contained" fullWidth size="large">
+            <Search /> Hae
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default SearchForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { GetStaticProps, NextApiResponse } from 'next';
-import { ChangeEvent, useEffect, useState, useRef } from 'react';
-import { Alert, Autocomplete, Button, Grid, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Alert, Button, Grid, Typography } from '@mui/material';
 import { databaseHoc, getCompanies, NextRequestWithDb } from 'utils/database';
-import cities from 'enums/cities.json';
-import certificates from 'enums/certificates.json';
 import CompanyListItem from 'components/CompanyListItem';
-import { Print, Search } from '@mui/icons-material';
+import { Print } from '@mui/icons-material';
 import useLocalStorage from 'hooks/useLocalStorage';
+import SearchForm from 'components/SearchForm';
 
 interface HomeProps {
   firstCompanies: Company[];
@@ -17,28 +16,6 @@ const Home = ({ firstCompanies, initialResultsAmount }: HomeProps) => {
   const [companies, setCompanies] = useState(firstCompanies);
   const [resultTotal, setResultTotal] = useState(initialResultsAmount);
   const [showInfo, setShowInfo] = useLocalStorage('showInfo', true);
-  const certRef = useRef<string | null>(null);
-
-  const onSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const query = ['certificate', 'name', 'city'].reduce((result: string[], key) => {
-      const input = event.target?.[key] as HTMLInputElement;
-      const value = input?.value;
-      if (value) {
-        if (key === 'certificate') {
-          result.push(`${key}=${certRef.current}`);
-        } else {
-          result.push(`${key}=${value}`);
-        }
-      }
-      return result;
-    }, []);
-
-    const result = await fetch(`/api/data?${query.join('&')}`);
-    const { data, totalResults } = await result.json();
-    setCompanies(data);
-    setResultTotal(totalResults);
-  };
 
   return (
     <main>
@@ -48,50 +25,7 @@ const Home = ({ firstCompanies, initialResultsAmount }: HomeProps) => {
           joko yrityksen nimen, y-tunnuksen, kaupungin tai sertifikaatin perusteella.
         </Alert>
       )}
-      <form onSubmit={onSubmit}>
-        <Typography component="h2" variant="h6" sx={{ pt: '10px' }}>
-          Rajaa tuloksia
-        </Typography>
-        <Grid
-          container
-          spacing={2}
-          alignItems="center"
-          sx={{ pt: '5px', pb: '15px', borderBottom: '1px solid #C4C4C4' }}
-        >
-          <Grid item xs={8} sm={5}>
-            <TextField id="name" fullWidth label="Yrityksen nimi tai y-tunnus" />
-          </Grid>
-          <Grid item xs={4} sm={3}>
-            <Autocomplete
-              disableClearable
-              fullWidth
-              id="certificate"
-              options={certificates}
-              getOptionLabel={(option) => option.name}
-              renderInput={(params) => <TextField {...params} label="Sertifikaatti" />}
-              onChange={(event, newValue) => {
-                if (certRef && 'current' in certRef) {
-                  certRef.current = newValue.id;
-                }
-              }}
-            />
-          </Grid>
-          <Grid item xs={8} sm={3}>
-            <Autocomplete
-              disableClearable
-              fullWidth
-              id="city"
-              options={cities.cities}
-              renderInput={(params) => <TextField {...params} label="Kaupunki" />}
-            />
-          </Grid>
-          <Grid item xs={4} sm={1}>
-            <Button type="submit" variant="contained" fullWidth size="large">
-              <Search /> Hae
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      <SearchForm setCompanies={setCompanies} setResultTotal={setResultTotal} />
       <Grid container item sx={{ my: '15px' }}>
         <Typography component="h3" variant="h5" sx={{ mr: '10px' }}>
           Hakutulokset ({resultTotal} kpl)
