240ab3709c987647b86eba48b989e5545719d55e
Petro Silenius <petro.silenius@gmail.com>
2022-02-02 23:13:00 +0200

    Separate certificate rendering in its own component, show stf separately in company result page

--- components/CompanyListItem.tsx
+++ components/CompanyListItem.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { Grid, Link as MuiLink, Typography } from '@mui/material';
 import certificates from 'enums/certificates.json';
+import CertificateItem from 'components/CertificateItem';
 
 const capitalizeFirstLetter = (string: string) => {
   if (!string) return null;
@@ -23,13 +24,7 @@ const CompanyListItem = ({ company }: { company: Company }) => {
           {company.certificateId?.map((id) => {
             const certificate = certificates.find((certificate) => certificate.id === id);
             if (certificate) {
-              return (
-                <Link href={`/sert/${certificate.id}`} passHref key={certificate.id}>
-                  <MuiLink>
-                    <img src={certificate.logoUrl} alt={certificate.name} height="50px" />
-                  </MuiLink>
-                </Link>
-              );
+              return <CertificateItem certificate={certificate} />;
             }
           })}
         </Grid>
