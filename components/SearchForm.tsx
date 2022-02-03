10012323c223100861e6e5316b3e4da839c3059d
Petro Silenius <petro.silenius@gmail.com>
2022-02-03 09:39:41 +0200

    Prefill search with query parameter cert, search if query param is set

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,8 +1,10 @@
-import { Alert, Autocomplete, Button, Grid, TextField, Typography } from '@mui/material';
-import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState, useRef } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Autocomplete, Button, Grid, TextField, Typography } from '@mui/material';
+import { FormEvent, Dispatch, SetStateAction, useEffect, useState } from 'react';
 import cities from 'enums/cities.json';
 import certificates from 'enums/certificates.json';
 import { Search } from '@mui/icons-material';
+import { useRouter } from 'next/router';
 
 const SearchForm = ({
   setCompanies,
@@ -11,22 +13,41 @@ const SearchForm = ({
   setCompanies: Dispatch<SetStateAction<Company[]>>;
   setResultTotal: Dispatch<SetStateAction<number>>;
 }) => {
+  const router = useRouter();
   const [company, setCompany] = useState('');
-  const [certs, setCerts] = useState<{ id: string; name: string }[]>([]);
+  const [certs, setCerts] = useState<Certificate[]>([]);
   const [areas, setAreas] = useState<{ id: string; name: string }[]>([]);
+  const [fetchPreSelected, setFetchPreSelected] = useState(false);
 
-  const onSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  useEffect(() => {
+    if (router.isReady && router.query.cert) {
+      const preSelectedCert: Certificate | undefined = certificates.find(
+        (certificate) => certificate.id === router.query.cert
+      );
+      preSelectedCert && setCerts([preSelectedCert]);
+      setFetchPreSelected(true);
+    }
+  }, [router.isReady]);
+
+  useEffect(() => {
+    if (fetchPreSelected && certs[0].id === router.query.cert) {
+      onSubmit();
+      setFetchPreSelected(false);
+    }
+  }, [fetchPreSelected]);
+
+  const onSubmit = async (event?: FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
     let query = [];
 
     if (company !== '') {
       query.push(`name=${company}`);
     }
-    if (certs !== []) {
+    if (certs.length !== 0) {
       const certIds = certs.map((cert) => cert.id);
       query.push(`certificate=${certIds}`);
     }
-    if (areas !== []) {
+    if (areas.length !== 0) {
       const areaIds = areas.map((area) => area.id);
       query.push(`city=${areaIds}`);
     }
