ae2703e02e5aa4b236af56157a17e3bd753ed502
Aleksi Väisänen <aleksi.vaisanen@loikka.io>
2022-02-05 14:09:47 +0200

    Remove duplicate scrapers and fine tune generalized functions

--- services/scrapers/scraper.ts
+++ services/scrapers/scraper.ts
@@ -15,20 +15,30 @@ type Processors = {
  * @returns status as boolean, true = ok
  */
 export const scraper = async (db: Knex<any, unknown[]>, dataSource: string) => {
+  let currentConfig;
   for (let i = 0; i < 5; i++) {
     try {
       const configs = scrapersConfig?.filter((conf) => conf?.id === dataSource);
       for (const config of configs) {
+        currentConfig = config;
         const responses = await fetch(config.url);
-        const html = await responses.text();
+        
+        let input;
+        if (config.inputType === "html") {
+          input = await responses.text();
+        } else {
+          input = await responses.json();
+        }
         const dataProcessor = (processors as Processors)[config.scraper];
-        const data = dataProcessor(html);
+        const data = dataProcessor(input);
 
         await upsertCompanyCertificates(data, db);
       }
       return true;
     } catch (error) {
-      console.error('Something went wrong with scrapeEkokompassi, try again after delay');
+      console.error(
+        `Something went wrong with data source "${currentConfig?.id}" in processor "${currentConfig?.scraper}", try again after delay`
+      );
       console.error(getErrorMessage(error));
       await sleep(30 * 1000);
     }
