331255ef0b553655e156f5ec2726d6e9aef8fee7
Aleksi Väisänen <aleksi.vaisanen@loikka.io>
2022-02-05 19:33:44 +0200

    SER-21 Add EMAS scraper

--- services/scrapers/index.ts
+++ services/scrapers/index.ts
@@ -39,6 +39,7 @@ export const processGreenKeyMain = (html: string): ApiCompanyCertificate[] => {
 export const processTourCert = (html: string): ApiCompanyCertificate[] => {
   const $ = cheerio.load(html);
   const output: ApiCompanyCertificate[] = [];
+
   $('.ajax_posts > .community-card .community-card-content h4.red.centered').each((index, node) => {
     // Trim whitespace
     const companyName = $(node).text().trim();
@@ -46,12 +47,14 @@ export const processTourCert = (html: string): ApiCompanyCertificate[] => {
       output.push({ companyName, certificateId: 'tour-cert' });
     }
   });
+
   return output;
 };
 
 export const processWWFGreenOffice = (html: string): ApiCompanyCertificate[] => {
   const $ = cheerio.load(html);
   const output: ApiCompanyCertificate[] = [];
+
   $(
     '#content .container-fluid.text-block.go-pillar .text-block__text > p:nth-of-type(-n+2) > a'
   ).each((index, node) => {
@@ -61,6 +64,7 @@ export const processWWFGreenOffice = (html: string): ApiCompanyCertificate[] => 
       output.push({ companyName, certificateId: 'wwf-green-office' });
     }
   });
+
   return output;
 };
 
@@ -84,10 +88,12 @@ type GEOGolfDataType = {
 
 export const processGEOGolf = (json: GEOGolfDataType[]): ApiCompanyCertificate[] => {
   const data = json?.filter((company) => company.certified === 1 && company.country === 'Finland');
+
   const output: ApiCompanyCertificate[] = data.map((d) => ({
     companyName: d.club_name,
     certificateId: 'geo-golf',
   }));
+
   return output;
 };
 
@@ -120,9 +126,30 @@ export const processBlueFlag = (html: string): ApiCompanyCertificate[] => {
 export const processECEATSuomi = (html: string): ApiCompanyCertificate[] => {
   const $ = cheerio.load(html);
   const output: ApiCompanyCertificate[] = [];
+
   $('.content-column.two_third.last_column > ul > li a').each((index, node) => {
     // Trim and collapse whitespace and remove trailing –
     const companyName = $(node).text().trim().replace(/\s\s+/g, ' ').replace(/ –$/gm, '');
+    if (companyName) {
+      output.push({ companyName, certificateId: 'eceat-suomi' });
+    }
+  });
+
+  return output;
+};
+
+export const processEMAS = (html: string): ApiCompanyCertificate[] => {
+  const $ = cheerio.load(html);
+  const output: ApiCompanyCertificate[] = [];
+  const nodes = $(
+    '#Main_Main_divLevelPresentation > div > div > div > div.page-contents > table > tbody > tr > td:nth-child(3) > p:nth-child(1) > a'
+  );
+
+  nodes.each((index, node) => {
+    // Trim and collapse whitespace
+    const rawCompanyName = $(node).text().trim().replace(/\s\s+/g, ' ');
+    const companyName = rawCompanyName?.split(',')?.[0];
+
     if (companyName) {
       output.push({ companyName, certificateId: 'eceat-suomi' });
     }
--- utils/database.ts
+++ utils/database.ts
@@ -94,7 +94,7 @@ export const upsertCompanyCertificates = async (
     ?.map((company) => {
       const cert = companyCertificates.find(
         (cCert) =>
-          cCert?.companyName?.toLowerCase()?.replace(/ oy$| tmi$| ky$/, '') === company?.name?.toLowerCase()?.replace(/ oy$| tmi$| ky$/, '')
+          cCert?.companyName?.toLowerCase()?.replace(/ oy$| oy ab$| tmi$| ky$/, '') === company?.name?.toLowerCase()?.replace(/ oy$| oy ab$| tmi$| ky$/, '')
       );
       return { certificate_id: cert?.certificateId, company_id: company?.id };
     })
