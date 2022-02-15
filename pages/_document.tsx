e44447683a81116d6873abfe5b9caa46a3046c88
Petro Silenius <petro.silenius@gmail.com>
2022-02-15 22:02:53 +0200

    Add link tags to document head, prettify manifest.json

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,13 +10,16 @@ export default class MyDocument extends Document {
       <Html lang="fi">
         <Head>
           <meta name="description" content="Matkailualan sertifikaattilukija" />
-          {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
